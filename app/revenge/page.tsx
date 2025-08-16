8999c9343c73339f16e81797525a2e41634e3265
pradoemanuel171-ship-it <pradoemanuel171@gmail.com>
2025-08-16 02:52:50 -0300

    x

--- app/revenge/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function RevengePage() {
-  return (
-    <div className="space-y-4">
-      <h1 className="text-xl font-bold">Venganza</h1>
-      <div className="card">Próximamente</div>
-    </div>
-  )
-}
